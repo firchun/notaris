84f1c7e010c37979c53ec9851078c0cf723ddff4
firchun025 <firchun025@gmail.com>
2024-03-28 09:20:52 +0900

    pages

--- resources/views/pages/pengajuan_layanan.blade.php
+++ resources/views/pages/pengajuan_layanan.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.frontend.app')
 
 @section('content')
+    <section id="main-container" class="main-container">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-12">
+                    <h3 class="section-sub-title">{{ $title ?? '-' }}</h3>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/pages/tentang_kami.blade.php
+++ resources/views/pages/tentang_kami.blade.php
@@ -3,6 +3,11 @@
 @section('content')
     <section id="main-container" class="main-container">
         <div class="container">
+            <div class="row text-center">
+                <div class="col-12">
+                    <h3 class="section-sub-title">{{ $title ?? '-' }}</h3>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-lg-6">
                     <h3 class="column-title">Who We Are</h3>
