8de10fa5c1863ad6d64bcc7cb60bfb8c3edad291
firchun025 <firchun025@gmail.com>
2024-06-26 10:00:27 +0900

    add biaya

--- resources/views/pages/harga.blade.php
+++ resources/views/pages/harga.blade.php
@@ -10,4 +10,26 @@
             </div>
         </div>
     </section>
+    <section id="main-container" class="main-container">
+        <div class="container">
+            <div class="mb-3">
+                <h2>Biaya Pengurusan Notaris</h2>
+            </div>
+            <div class="mb-3 text-center">
+                <a href="{{ asset('/biaya/notaris.pdf') }}" class="btn btn-dark" download>Download Biaya Notaris</a>
+            </div>
+            <iframe src="{{ asset('/biaya/notaris.pdf') }}" style="width: 100%; height:500px;"></iframe>
+        </div>
+    </section>
+    <section id="main-container" class="main-container">
+        <div class="container">
+            <div class="mb-3">
+                <h2>Biaya Pengurusan PPAT</h2>
+            </div>
+            <div class="mb-3 text-center">
+                <a href="{{ asset('/biaya/ppat.pdf') }}" class="btn btn-dark" download>Download Biaya PPAT</a>
+            </div>
+            <iframe src="{{ asset('/biaya/ppat.pdf') }}" style="width: 100%; height:500px;"></iframe>
+        </div>
+    </section>
 @endsection
